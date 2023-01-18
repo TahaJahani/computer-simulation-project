91a524dc16decabb303016145190d984e77417ed
taha7900 <taha7900@gmail.com>
2023-01-19 02:51:52 +0330

    job creation logic completed Co-authored-by: shahabhm <shhm3834@gmail.com>

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from job import Job
 from heapq import heappush, heappop
 import numpy as np
 import math
-
-TIME = 0
-AVG_Y = 100
+import time
 
 class Cpu:
     priority_queue = []
@@ -15,8 +13,28 @@ class Cpu:
     def add_job(self, job: Job):
         heappush(self.priority_queue, job)
 
+
+TOTAL_TIME = 1000
+TIME = 0
+AVG_Y = 100
+RATE_X = 10
+cpu = Cpu()
+
 class Main:
-    def jobCreator(self) -> Job:
+    next_job_arriving_at = 0
+    def jobCreator(self) -> tuple[Job, int]:
+        interarrival_time = math.ceil(np.random.exponential(RATE_X)) # Or 1/X?
         service_time = math.ceil(np.random.poisson(AVG_Y))
         priority = np.random.choice([1,2,3], 1, p=[0.7, 0.2, 0.1])[0]
-        return Job(service_time, priority, TIME)
+        j =  Job(service_time, priority, TIME)
+        return j, interarrival_time
+    
+    def check_job_creation(self):
+        if TIME == self.next_job_arriving_at:
+            j, next_interarrival = self.jobCreator()
+            cpu.add_job(j)
+            self.next_job_arriving_at += next_interarrival
+    
+    def run_main_thread(self):
+        for TIME in range(TOTAL_TIME):
+            self.check_job_creation()
