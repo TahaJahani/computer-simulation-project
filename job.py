2487a727aef199452b90f31f05fdee3d45f4a617
taha7900 <taha7900@gmail.com>
2023-01-19 03:04:43 +0330

    dispacher and utility functions added to job Co-authored-by: shahabhm <shhm3834@gmail.com>

--- job.py
+++ job.py
@@ -8,9 +8,20 @@ class JOB_PRIORITY(enum.Enum):
 
 class Job:
     creation_time = None
-    start_time = None
     service_time:int = None
+    executed_time: int = 0
     priority: JOB_PRIORITY = None
+
+    def execute(self, time):
+        self.executed_time += time
+    
+
+    def time_remaining(self) -> int:
+        return self.service_time - self.executed_time
+    
+
+    def is_finished(self):
+        return self.time_remaining() == 0
 
     def __init__(self, service_time, priority, creation_time) -> None:
         self.service_time = service_time
--- main.py
+++ main.py
@@ -12,15 +12,37 @@ class Cpu:
 
     def add_job(self, job: Job):
         heappush(self.priority_queue, job)
+    
+
+    def move_job_to_roundrobinT1(self):
+        j = heappop(self.priority_queue)
+        self.roundrobinT1.append(j)
+
+    
+    def dispatcher(self):
+        to_execute :Job = None
+        if len(self.roundrobinT1) != 0:
+            to_execute = self.roubrobinT1.pop(0)
+        elif len(self.roubrobinT2) != 0:
+            to_execute = self.roubrobinT2.pop(0)
+        elif len(self.fcfs) != 0:
+            to_execute = self.fcfs.pop(0)
+        # Execute the job
+        pass
+    
+
+    def count_jobs_in_queues(self):
+        return len(self.priority_queue) + len(self.roundrobinT1) + len(self.roubrobinT2)
 
 
 TOTAL_TIME = 1000
 TIME = 0
 AVG_Y = 100
 RATE_X = 10
-cpu = Cpu()
+LEN_K = 8
 
 class Main:
+    cpu = Cpu()
     next_job_arriving_at = 0
     def jobCreator(self) -> tuple[Job, int]:
         interarrival_time = math.ceil(np.random.exponential(RATE_X)) # Or 1/X?
@@ -29,12 +51,21 @@ class Main:
         j =  Job(service_time, priority, TIME)
         return j, interarrival_time
     
+
+    def jobLoader(self):
+        if self.cpu.count_jobs_in_queues() < LEN_K:
+            for i in range(LEN_K):
+                self.cpu.move_job_to_roundrobinT1()
+    
     def check_job_creation(self):
         if TIME == self.next_job_arriving_at:
             j, next_interarrival = self.jobCreator()
-            cpu.add_job(j)
+            self.cpu.add_job(j)
             self.next_job_arriving_at += next_interarrival
+
     
     def run_main_thread(self):
         for TIME in range(TOTAL_TIME):
             self.check_job_creation()
+            self.jobLoader()
+            self.cpu.dispatcher()
